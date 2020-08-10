74a17fb4221e1b3cb551e9f17ab773bb125e4b8b
Diego Merello <diego@wipitalia.it>
2020-08-10 19:45:15 +0200

    prepared twenmax animatin on click

--- src/js/components/Paradise.js
+++ src/js/components/Paradise.js
@@ -1,18 +1,27 @@
+import { TweenMax, Strong, CSSPlugin } from "gsap";
+
 export default class Paradise {
   constructor(wrapperId){
     this.wrapper = document.getElementById(wrapperId);
     this.dot = this.wrapper.querySelector(`.paradise__cursor`);
     this.curve = this.wrapper.querySelector(`.paradise__path`);
     this.scroller = this.wrapper.querySelector(`.paradise__scroller`);
-
+    this.menuItems = document.querySelectorAll(`.primary-menu a`);
 
     this.totalLength = this.curve.getTotalLength();
     this.startingPosition = 0;
     this.setPosition(this.startingPosition);
 
     this.wrapper.addEventListener('scroll', ()=>{
-      this.initInteraction();
+      this.initCursorInteraction();
     });
+
+    this.menuItems.forEach((el, index)=>{
+      el.addEventListener('click', (e)=>{
+        this.setPositionOnClick(e);
+      });
+    });
+
   }
 
   setPosition(position){
@@ -20,14 +29,23 @@ export default class Paradise {
     this.dot.setAttribute("transform", `translate(${p.x}, ${p.y})`);
   }
 
-
-  initInteraction(e){
+  initCursorInteraction(e){
     let toScroll = (this.scroller.offsetHeight - this.wrapper.offsetHeight) ;
     let scrolled = this.wrapper.scrollTop;
-    console.log(toScroll, scrolled);
     let perc = (scrolled * 100) / toScroll ;
     let position = perc/100;
     this.setPosition(position);
+  }
+
+  setPositionOnClick(e){
+    e.preventDefault();
+    let position = e.target.dataset.position;
+    let p = this.curve.getPointAtLength( position *  this.totalLength);
+    console.log(p);
+    this.animation = TweenMax.to(this.dot, 1, {
+      transform: `translate(${p.x}, ${p.y})`,
+      ease:Strong.easeOut
+    });
   }
 }
 
